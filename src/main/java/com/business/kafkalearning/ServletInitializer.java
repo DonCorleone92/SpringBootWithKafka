8ca8237a8b9905ad6a31e44e475792531cfdb8cd
Singhal <abhinav.singhal02@sap.com>
2020-10-05 13:19:02 +0530

    Commiting the base code structure

---
+++ src/main/java/com/business/kafkalearning/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.business.kafkalearning;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KafkalearningApplication.class);
+	}
+
+}
